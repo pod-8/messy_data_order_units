c3eb74a9ac4ce9b7b9377af74820080f05d92a19
Richard Warburton <admin@pod8.co.uk>
2023-07-21 12:16:15 +0100

    Scripts and notebooks written

---
+++ CleanMessyData.py
@@ -0,0 +1,17 @@
+# Import packages required
+import pandas as pd
+
+# Import messy data from Excel and assign to the mesy_data variabe as a data frame
+messy_data = pd.read_excel('messy_order_data.xlsx', sheet_name='Sheet1')
+
+# Cleaning step 1: Remove ',' from the values and replace with ''
+messy_data['Price Per Unit'] = messy_data['Price Per Unit'].str.replace(',','')
+
+# Cleaning Step 2: Remove '£---' from the values and replace with ''
+messy_data['Price Per Unit'] = messy_data['Price Per Unit'].str.replace('£---','')
+
+# Cleaning Step 3: Coerce Price Per Unit to be a float 
+messy_data['Price Per Unit'] = messy_data['Price Per Unit'].astype(float)
+
+# Export data back to excel (index = False ensures the pandas indicies are not exported to excel)
+messy_data.to_excel('clean_order_data.xlsx',index=False)
